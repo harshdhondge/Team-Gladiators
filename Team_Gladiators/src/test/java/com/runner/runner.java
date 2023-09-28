b447d9b0570ab697112e0e316277c03e87fef43c
Harshad Dhondge <102278515+harshdhondge@users.noreply.github.com>
2023-09-28 12:54:24 +0530

    Resolved merge conflict by incorporating both suggestions.

--- Team_Gladiators/src/test/java/com/runner/runner.java
+++ Team_Gladiators/src/test/java/com/runner/runner.java
@@ -8,6 +8,15 @@ import io.cucumber.testng.CucumberOptions;
 
 public class runner {
 
+	@CucumberOptions(tags = "",
+			features = "src\\test\\resources\\Features\\LoginFunctionality.feature",
+			glue = "com.stepdefination",
+			plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+ 
+
+	public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+ 
+
 	}
 
 }
